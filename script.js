6b800bb246cb8a7e8ba626c5f598b853875f23d4
DanFromUkraine <126253915+DanFromUkraine@users.noreply.github.com>
2024-04-05 10:19:26 +0300

    please

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-
-var fade = function  (node) {
+var fade = function(node) {
     var level = 1;
-    var step = function () {
+    var step = function() {
         var hex = level.toString(16);
         node.style.backgroundColor = '#FFFF' + hex + hex;
         if (level < 15) {
             level += 1;
             setTimeout(step, 100);
         }
-    }
-    setTimeout(step, 100);
-}
+    };
+    step(); // Викликаємо функцію step, щоб розпочати анімацію при виклику fade
+};
 
-fade(document.body);
+setTimeout(function() {
+    fade(document.body);
+}, 1000); // Запускаємо анімацію через 1 секунду після виклику setTimeout
